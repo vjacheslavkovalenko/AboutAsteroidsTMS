e2af062a7f78105ac849dd49bf810ce728bfade9
vjacheslavkovalenko <vjacheslavkovalenko@gmail.com>
2024-12-09 19:44:29 +0300

    Commit N19. Add navigation.

--- app/src/main/java/by/vjacheslavkovalenko/aboutasteroidstms/ui/MainFragment.kt
+++ app/src/main/java/by/vjacheslavkovalenko/aboutasteroidstms/ui/MainFragment.kt
@@ -8,35 +8,40 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.vjacheslavkovalenko.aboutasteroidstms.R
 import by.vjacheslavkovalenko.aboutasteroidstms.databinding.FragmentMainBinding
+import by.vjacheslavkovalenko.aboutasteroidstms.ui.pictureofday.PictureOfDayFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     private var binding: FragmentMainBinding? = null
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMainBinding.inflate(inflater, container, false)
+//        binding = FragmentMainBinding.inflate(inflater, container, false)
+        binding = FragmentMainBinding.inflate(inflater)
         return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Устанавливаем обработчик для кнопки перехода на AsteroidsByDateFragment
+        if (savedInstanceState == null) {
+            val pictureOfDayFragment = PictureOfDayFragment()
+            childFragmentManager.beginTransaction()
+                .replace(R.id.picture_of_day_container, pictureOfDayFragment) // Замените на ваш ID контейнера
+                .commit()
+        }
+
         binding?.buttonAsteroidsByDate?.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_asteroidsByDateFragment)
         }
 
-        // Устанавливаем обработчик для кнопки перехода на WebViewFragment
-        binding?.buttonWebView?.setOnClickListener {
-            findNavController().navigate(R.id.action_mainFragment_to_webViewFragment)
+        binding?.buttonAboutApp?.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_aboutAppFragment)
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        binding = null // Освобождаем ресурсы
     }
 }
