ca27d0c4bab253fa8ba45db5273f814354047132
vjacheslavkovalenko <vjacheslavkovalenko@gmail.com>
2024-12-09 19:22:59 +0300

    Commit N18. Add json.

---
+++ app/src/main/java/by/vjacheslavkovalenko/aboutasteroidstms/ui/MainFragment.kt
@@ -0,0 +1,42 @@
+package by.vjacheslavkovalenko.aboutasteroidstms.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.vjacheslavkovalenko.aboutasteroidstms.R
+import by.vjacheslavkovalenko.aboutasteroidstms.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private var binding: FragmentMainBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Устанавливаем обработчик для кнопки перехода на AsteroidsByDateFragment
+        binding?.buttonAsteroidsByDate?.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_asteroidsByDateFragment)
+        }
+
+        // Устанавливаем обработчик для кнопки перехода на WebViewFragment
+        binding?.buttonWebView?.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_webViewFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null // Освобождаем ресурсы
+    }
+}
